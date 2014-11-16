9debe97277f9aea2fd6208a50bdb9c12f0124439
cxb <lakayiiii@gmail.com>
2014-11-16 03:31:05 -0500

    login/password empty check

--- src/com/example/danceforhealth/LoginDialog.java
+++ src/com/example/danceforhealth/LoginDialog.java
@@ -4,10 +4,14 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnShowListener;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnCreateContextMenuListener;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,12 +19,16 @@ public class LoginDialog extends DialogFragment{
 	
 	private EditText username;
 	private EditText password;
+	private View view;
+	private Dialog mDialog;
+
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		LayoutInflater inflater = getActivity().getLayoutInflater();
 		View view = inflater.inflate(R.layout.popup_window_login, null);
+		this.view = view;
 		username = (EditText) view.findViewById(R.id.usernameEditText);
 		password = (EditText) view.findViewById(R.id.passwordEditText);
 		builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -35,13 +43,88 @@ public class LoginDialog extends DialogFragment{
 			
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
+				
+				
+			}
+		});
+	
+		builder.setView(view);
+		Dialog dialog = builder.create();
+		mDialog = dialog;
+		
+		((AlertDialog) dialog).setOnShowListener(new OnShowListener() {
+			
+			@Override
+			public void onShow(DialogInterface dialog) {
+				
+				((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+				
+			}
+		});
+		
+		username.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				if (!usernameOrPasswordEmpty()){
+					((AlertDialog) mDialog).getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
+				}else{
+					((AlertDialog) mDialog).getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+				}
+				
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
 				// TODO Auto-generated method stub
 				
 			}
 		});
 		
-		builder.setView(view);
-		Dialog dialog = builder.create();
+		password.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				if (!usernameOrPasswordEmpty()){
+					((AlertDialog) mDialog).getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
+				}else{
+					((AlertDialog) mDialog).getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+				}
+				
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
 		return dialog;
 	}	
+	
+	private boolean usernameOrPasswordEmpty(){
+		username = (EditText) view.findViewById(R.id.usernameEditText);
+		password = (EditText) view.findViewById(R.id.passwordEditText);
+		if (username.getText().toString().trim().equals("") || password.getText().toString().trim().equals("")){
+			return true;
+		}
+		return false;
+	}
 }
